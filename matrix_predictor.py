52c9763a81fc6dbcf217fff6bed420528c04ef13
Devin Morgan <devinm@mit.edu>
2018-04-13 21:31:42 -0400

    Created the RNN

--- matrix_predictor.py
+++ matrix_predictor.py
@@ -1,57 +1,50 @@
-# 1) train a model that reads in an AST (as DFS sequence) and then outputs a matrix
-# 2) one way to approach this is by only taking the last output of the RNN......
-
-# 3) what form is the data in? (filename=id --> Matrix, asts[id] = AST)
-#   (Matrix, AST) --> (Matrix, sequence_AST)
-# iterate over sequence_AST and feed it to the RNN. Save the last output of the RNN as the predicted matrix
-
-# I want to have data in batches that I can just feed to the model
-
-import os, sys
 import tensorflow as tf
-from brians_code import ast_to_sequence, MAX_SEQUENCE_LENGTH, STATEMENT_DIMENSION
 
-from matrix_learner import H1_UNITS
-from matrix_learner_tf_records import parse_ast_data, MATRICES_DIR
+from matrix_predictor_tf_records import TF_RECORD_FILE, tf_record_parser, STATEMENT_DIMENSION, MAX_SEQUENCE_LENGTH, H1_UNITS
 
 
-TF_RECORD_FILE = "Datasets/Hour of Code/matrix_predictor.tfrecord"
+BATCH_SIZE = 64
+HIDDEN_STATE_SIZE = [200, 100, 40]
+NUM_LSTM_LAYERS = 3
+NUM_EPOCHS = 100
+
+data_iter = tf.data.TFRecordDataset(TF_RECORD_FILE)\
+            .map(tf_record_parser)\
+            .batch(BATCH_SIZE)\
+            .make_initializable_iterator()
+sequences, matrices = data_iter.get_next()
 
 
-def cond_tf_record_parser(record):
-    keys_to_features = {
-        "sequence": tf.FixedLenFeature([STATEMENT_DIMENSION, MAX_SEQUENCE_LENGTH], tf.float32),
-        "matrix": tf.FixedLenFeature([H1_UNITS, H1_UNITS], tf.float32),
-    }
-    parsed = tf.parse_single_example(record, keys_to_features)
-    return parsed['precond'], parsed['postcond']
+def multi_lstm_model():
+    cells = [tf.nn.rnn_cell.BasicLSTMCell(size, state_is_tuple=True) for size in HIDDEN_STATE_SIZE]
+    return tf.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=True)
 
 
-def _floats_feature(value):
-    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+Seqs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_SEQUENCE_LENGTH, STATEMENT_DIMENSION])
+Mats = tf.placeholder(tf.float32, [BATCH_SIZE, H1_UNITS, H1_UNITS])
 
+lstm_model = multi_lstm_model()
+output, state = tf.nn.dynamic_rnn(lstm_model, Seqs)
+predicted_matrices = tf.gather(output, [MAX_SEQUENCE_LENGTH - 1], axis=1)
 
-def write_to_tf_record(writer, sequence, matrix):
-    feature = {
-        'sequence': _floats_feature(sequence),
-        'matrix': _floats_feature(matrix),
-    }
-    example = tf.train.Example(features=tf.train.Features(feature=feature))
-    writer.write(example.SerializeToString())
+loss = tf.losses.mean_squared_error(Mats, predicted_matrices)
+optimizer = tf.train.AdamOptimizer().minimize(loss)
 
-
-if __name__ == "main":
-    ast_to_id, asts = parse_ast_data()
-    writer = tf.python_io.TFRecordWriter(TF_RECORD_FILE)
-    for id in range(len(asts)):
-        tf.reset_default_graph()
-        matrix = tf.get_variable("linear_map", shape=[H1_UNITS, H1_UNITS])
-        matrix_file_path = os.path.join(MATRICES_DIR, "{}.ckpt".format(id))
-        saver = tf.train.Saver()
-        with tf.Session() as sess:
-            saver.restore(sess, matrix_file_path)
-            var_mat = matrix.eval()
-            ast_seq = ast_to_sequence(asts[id])
-            write_to_tf_record(writer, ast_seq, var_mat)
-    writer.close()
-    sys.stdout.flush()
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(NUM_EPOCHS):
+        sess.run(data_iter.initializer)
+        while True:
+            try:
+                _, train_loss_val = sess.run([optimizer, loss], feed_dict={
+                    Seqs: sess.run(sequences),
+                    Mats: sess.run(matrices)
+                })
+                print('File: {} Epoch {}: Minibatch Loss: {}'.format(file, epoch, train_loss_val))
+            except tf.errors.OutOfRangeError:
+                break
+            except tf.errors.InvalidArgumentError:
+                break  # typically happens when there isn't enough data for a given AST
+        # TODO: compute loss on eval dataset
+# TODO: computer loss on test dataset
---
+++ matrix_predictor_tf_records.py
@@ -0,0 +1,57 @@
+# 1) train a model that reads in an AST (as DFS sequence) and then outputs a matrix
+# 2) one way to approach this is by only taking the last output of the RNN......
+
+# 3) what form is the data in? (filename=id --> Matrix, asts[id] = AST)
+#   (Matrix, AST) --> (Matrix, sequence_AST)
+# iterate over sequence_AST and feed it to the RNN. Save the last output of the RNN as the predicted matrix
+
+# I want to have data in batches that I can just feed to the model
+
+import os, sys
+import tensorflow as tf
+from brians_code import ast_to_sequence, MAX_SEQUENCE_LENGTH, STATEMENT_DIMENSION
+
+from matrix_learner import H1_UNITS
+from matrix_learner_tf_records import parse_ast_data, MATRICES_DIR
+
+
+TF_RECORD_FILE = "Datasets/Hour of Code/matrix_predictor.tfrecord"
+
+
+def tf_record_parser(record):
+    keys_to_features = {
+        "sequence": tf.FixedLenFeature([STATEMENT_DIMENSION, MAX_SEQUENCE_LENGTH], tf.float32),
+        "matrix": tf.FixedLenFeature([H1_UNITS, H1_UNITS], tf.float32),
+    }
+    parsed = tf.parse_single_example(record, keys_to_features)
+    return parsed['sequence'], parsed['matrix']
+
+
+def _floats_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def write_to_tf_record(writer, sequence, matrix):
+    feature = {
+        'sequence': _floats_feature(sequence),
+        'matrix': _floats_feature(matrix),
+    }
+    example = tf.train.Example(features=tf.train.Features(feature=feature))
+    writer.write(example.SerializeToString())
+
+
+if __name__ == "main":
+    ast_to_id, asts = parse_ast_data()
+    writer = tf.python_io.TFRecordWriter(TF_RECORD_FILE)
+    for id in range(len(asts)):
+        tf.reset_default_graph()
+        matrix = tf.get_variable("linear_map", shape=[H1_UNITS, H1_UNITS])
+        matrix_file_path = os.path.join(MATRICES_DIR, "{}.ckpt".format(id))
+        saver = tf.train.Saver()
+        with tf.Session() as sess:
+            saver.restore(sess, matrix_file_path)
+            var_mat = matrix.eval()
+            ast_seq = ast_to_sequence(asts[id])  # assume output is STATEMENT_DIMENSION x MAX_SEQUENCE_LENGTH
+            write_to_tf_record(writer, ast_seq, var_mat)
+    writer.close()
+    sys.stdout.flush()
