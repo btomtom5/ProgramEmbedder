a9e6103408e8bfaf9410a958d827949a91c98a3f
Devin Morgan <devinm@mit.edu>
2018-04-18 11:16:39 -0400

    Created seperate directory for hoare_triples_tf_records for safety/organization. Finished debuggin neural_predictor_tf_records

--- neural_predictor_tf_records.py
+++ neural_predictor_tf_records.py
@@ -2,14 +2,16 @@ import os, sys, json
 import tensorflow as tf
 import numpy as np
 
+from random import shuffle
+
 from ast_tokenizer import vectorize_token_list as ast_to_sequence, NUM_TOKENS as TOKEN_DIMENSION
 from matrix_learner_tf_records import PRECONDITION, POSTCONDITION, AST, COND_FEATURE_LENGTH
-from matrix_predictor import MAX_SEQUENCE_LENGTH
+from matrix_predictor_tf_records import MAX_SEQUENCE_LENGTH
 
 HOARE_TRIPLES_DIR = "Datasets/hour_of_code/dev_data/hoare_triples"
-TF_RECORDS_TRAIN = "Datasets/hour_of_code/dev_data/tf_records/train.tfrecord"
-TF_RECORDS_EVAL = "Datasets/hour_of_code/dev_data/tf_records/eval.tfrecord"
-TF_RECORDS_TEST = "Datasets/hour_of_code/dev_data/tf_records/test.tfrecord"
+TF_RECORDS_TRAIN = "Datasets/hour_of_code/dev_data/hoare_triples_tf_records/train.tfrecord"
+TF_RECORDS_EVAL = "Datasets/hour_of_code/dev_data/hoare_triples_tf_records/eval.tfrecord"
+TF_RECORDS_TEST = "Datasets/hour_of_code/dev_data/hoare_triples_tf_records/test.tfrecord"
 
 
 def tf_record_parser(serialized_example):
@@ -42,19 +44,32 @@ def write_hoare_triple_to_tf_record(json_hoare, tf_writer):
         # use the proto object to serialize the example to a string
         serialized = example.SerializeToString()
         # write the serialized object to disk
-        writer.write(serialized)
+        tf_writer.write(serialized)
+
+
+def create_tf_record_from_hoare_record_paths(hoare_triples_paths, tf_record_path):
+    writer = tf.python_io.TFRecordWriter(tf_record_path)
+    for ht_path in hoare_triples_paths:
+        with open(ht_path, 'r') as json_file:
+            for line in json_file:
+                write_hoare_triple_to_tf_record(line.strip(), writer)
+    writer.close()
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
-    writer = tf.python_io.TFRecordWriter(TF_RECORDS_FILE)
+    hoare_triples = []
     for root, dirs, files in os.walk(HOARE_TRIPLES_DIR):
         for file in files:
             if file.endswith(".json"):
                 print("Json file: {}".format(file))
                 json_file_path = os.path.join(root, file)
-                json_asts = []
-                with open(json_file_path, 'r') as json_file:
-                    for line in json_file:
-                        write_hoare_triple_to_tf_record(line.strip(), writer)
-    writer.close()
-    sys.stdout.flush()
+                hoare_triples.append(json_file_path)
+    shuffle(hoare_triples)
+    train_frac, eval_frac = int(0.8 * len(hoare_triples)), int(0.9 * len(hoare_triples))
+    train_triples = hoare_triples[:train_frac]
+    eval_triples = hoare_triples[train_frac: eval_frac]
+    test_triples = hoare_triples[eval_frac:]
+    create_tf_record_from_hoare_record_paths(train_triples, TF_RECORDS_TRAIN)
+    create_tf_record_from_hoare_record_paths(eval_triples, TF_RECORDS_EVAL)
+    create_tf_record_from_hoare_record_paths(test_triples, TF_RECORDS_TEST)
